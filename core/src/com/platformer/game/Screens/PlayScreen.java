c4465cc10b5617dc857b184298cc2ba5bdd55f3f
Jia Ming <jma8774@bths.edu>
2017-05-23 23:14:08 -0400

    created box2d fixtures/body for the rectangles and cancer polyline spikes

--- core/src/com/platformer/game/Screens/PlayScreen.java
+++ core/src/com/platformer/game/Screens/PlayScreen.java
@@ -6,15 +6,21 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.EllipseMapObject;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Polyline;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
@@ -43,12 +49,12 @@ public class PlayScreen implements Screen {
     private Box2DDebugRenderer b2dr;
 
 
-    public PlayScreen(platformerGame game){
+    public PlayScreen(platformerGame game) {
         this.game = game;
         //cam to follow user through map
         gamecam = new OrthographicCamera();
         //maintain aspect ratio for screens
-        port = new FitViewport(platformerGame.gameWidth,platformerGame.gameHeight,gamecam);
+        port = new FitViewport(platformerGame.gameWidth, platformerGame.gameHeight, gamecam);
         //adds a hud
         hud = new Hud(game.batch);
 
@@ -56,14 +62,10 @@ public class PlayScreen implements Screen {
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("gamemap.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
-        gamecam.position.set(port.getWorldWidth()/2,32000-(port.getWorldHeight()/2),0);
-
-        //-----------------------MARIO TEST-----------------------------------//
-//        gamecam.position.set(port.getWorldWidth()/2,port.getWorldHeight()/2,0);
-        //-----------------------MARIO TEST-----------------------------------//
+        gamecam.position.set(port.getWorldWidth() / 2, 32000 - (port.getWorldHeight() / 2), 0);
 
         //box2d
-        world = new World(new Vector2(0,0),true);
+        world = new World(new Vector2(0, 0), true);
         b2dr = new Box2DDebugRenderer();
         BodyDef bdef = new BodyDef();
         PolygonShape shape = new PolygonShape();
@@ -71,58 +73,30 @@ public class PlayScreen implements Screen {
         Body body;
         //gets certain objects by layer used for collision
 
+        //body/fixtures for rectangular object layers
+        for(int i = 0; i < 11; i ++) {
+            for (MapObject object : map.getLayers().get(i).getObjects().getByType(RectangleMapObject.class)) {
+                Rectangle rect = ((RectangleMapObject) object).getRectangle();
+                bdef.type = BodyDef.BodyType.StaticBody;
+                bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+                body = world.createBody(bdef);
+                shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+                fdef.shape = shape;
+                body.createFixture(fdef);
+            }
+        }
 
-        /*-------------------------------------MARIO TEST GAME--------------------------------------------*/
-//        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
-//            Rectangle rect = ((RectangleMapObject)object).getRectangle();
-//            //setting the player
-//            bdef.type = BodyDef.BodyType.StaticBody;
-//            bdef.position.set(rect.getX() + rect.getY()/2, rect.getY() + rect.getHeight()/2);
-//            body = world.createBody(bdef);
-//            shape.setAsBox(rect.getWidth()/2,rect.getHeight()/2);
-//            fdef.shape = shape;
-//            body.createFixture(fdef);
-//        }
-//        for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
-//            Rectangle rect = ((RectangleMapObject)object).getRectangle();
-//            //setting the player
-//            bdef.type = BodyDef.BodyType.StaticBody;
-//            bdef.position.set(rect.getX() + rect.getY()/2, rect.getY() + rect.getHeight()/2);
-//            body = world.createBody(bdef);
-//            shape.setAsBox(rect.getWidth()/2,rect.getHeight()/2);
-//            fdef.shape = shape;
-//            body.createFixture(fdef);
-//        }
-//        for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
-//            Rectangle rect = ((RectangleMapObject)object).getRectangle();
-//            //setting the player
-//            bdef.type = BodyDef.BodyType.StaticBody;
-//            bdef.position.set(rect.getX() + rect.getY()/2, rect.getY() + rect.getHeight()/2);
-//            body = world.createBody(bdef);
-//            shape.setAsBox(rect.getWidth()/2,rect.getHeight()/2);
-//            fdef.shape = shape;
-//            body.createFixture(fdef);
-//        }
-//        for(MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
-//            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-//            //setting the player
-//            bdef.type = BodyDef.BodyType.StaticBody;
-//            bdef.position.set(rect.getX() + rect.getY() / 2, rect.getY() + rect.getHeight() / 2);
-//            body = world.createBody(bdef);
-//            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
-//            fdef.shape = shape;
-//            body.createFixture(fdef);
-//        }
-        /*-------------------------------------MARIO TEST GAME--------------------------------------------*/
-
-        for(MapObject object : map.getLayers().get(11).getObjects().getByType(RectangleMapObject.class)){
-            System.out.println("ran");
-            Rectangle rect = ((RectangleMapObject)object).getRectangle();
+        //body/fixtures for polyline spikes CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF CANCER AF
+        for (MapObject object : map.getLayers().get(11).getObjects().getByType(PolylineMapObject.class)) {
+            Polyline poly = ((PolylineMapObject) object).getPolyline();
             bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set(rect.getX() + rect.getY()/2, rect.getY() + rect.getHeight()/2);
+            bdef.position.set(0, 0);
             body = world.createBody(bdef);
-            shape.setAsBox(rect.getWidth()/2,rect.getHeight()/2);
-            fdef.shape = shape;
+
+            float vertices[] = ((PolylineMapObject) object).getPolyline().getTransformedVertices();
+            ChainShape shape2 = new ChainShape();
+            shape2.createChain(vertices);
+            fdef.shape = shape2;
             body.createFixture(fdef);
         }
     }
@@ -132,31 +106,31 @@ public class PlayScreen implements Screen {
 
     }
 
-    public void update(float dt){
+    public void update(float dt) {
         handleInput(dt);
         gamecam.update();
         renderer.setView(gamecam);
     }
 
-    public void handleInput(float dt){
-        if(Gdx.input.isTouched())
-            gamecam.position.x += 1000*dt;
+    public void handleInput(float dt) {
+        if (Gdx.input.isTouched())
+            gamecam.position.x += 1000 * dt;
     }
 
     @Override
     public void render(float delta) {
         update(delta);
-        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderer.render();
-        b2dr.render(world,gamecam.combined);
+        b2dr.render(world, gamecam.combined);
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        port.update(width,height);
+        port.update(width, height);
     }
 
     @Override
