94e39f9ee82b6bbe2092d700f5e96dd5f7f3234b
billyluy <bluy0969@bths.edu>
2017-05-26 12:21:11 -0400

    Update Player.java

--- core/src/com/platformer/game/Sprites/Player.java
+++ core/src/com/platformer/game/Sprites/Player.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.platformer.game.platformerGame;
 
 /**
  * Created by Student6 on 5/24/2017.
@@ -23,13 +24,13 @@ public class Player extends Sprite {
     public void definePlayer(){
         BodyDef bdef = new BodyDef();
         //position is based on the entire map
-        bdef.position.set(128,31100);
+        bdef.position.set(128/platformerGame.PPM,31164/platformerGame.PPM);
         bdef.type = BodyDef.BodyType.DynamicBody;
         body = world.createBody(bdef);
 
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(50);
+        shape.setRadius(30/platformerGame.PPM);
         fdef.shape = shape;
         body.createFixture(fdef);
     }
