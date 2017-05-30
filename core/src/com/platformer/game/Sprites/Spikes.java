6cbfddc3e218aee225e41460a2293baa8826c437
billyluy <bluy0969@bths.edu>
2017-05-30 12:22:18 -0400

    code clean up

--- core/src/com/platformer/game/Sprites/Spikes.java
+++ core/src/com/platformer/game/Sprites/Spikes.java
@@ -1,8 +1,23 @@
 package com.platformer.game.Sprites;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
 /**
- * Created by Student6 on 5/26/2017.
+ * Created by Student6 on 5/30/2017.
  */
 
-public class Spikes {
+public class Spikes extends InteractiveTiles {
+    public Spikes(World world, TiledMap map, Rectangle bounds) {
+        super(world, map, bounds);
+        BodyDef bdef = new BodyDef();
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+
+
+    }
 }
