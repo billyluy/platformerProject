18032d986f7fcee6b112faf1a0543b79de406e54
Jia Ming <jma8774@bths.edu>
2017-05-30 12:56:17 -0400

    SUCCESSFULLY DETECTED COLLISION BETWEEN PLAYER AND SPIKES

---
+++ core/src/com/platformer/game/Tools/WorldContactListener.java
@@ -0,0 +1,37 @@
+package com.platformer.game.Tools;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+/**
+ * Created by Student 6 on 5/30/2017.
+ */
+
+public class WorldContactListener implements ContactListener {
+    @Override
+    public void beginContact(Contact contact) {
+        Fixture fixA = contact.getFixtureA();
+        Fixture fixB = contact.getFixtureB();
+        if(fixA != null && fixA.getUserData().equals("spike")){
+            System.out.println("HIT SPIKE");
+        }
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
