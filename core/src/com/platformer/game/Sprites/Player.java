c0d7f704470f69d053cb9e31c120818619e68cf5
Jia Ming <jma8774@bths.edu>
2017-06-03 18:53:29 -0400

    created the player sprite, should be perfect

--- core/src/com/platformer/game/Sprites/Player.java
+++ core/src/com/platformer/game/Sprites/Player.java
@@ -1,5 +1,6 @@
 package com.platformer.game.Sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -8,6 +9,7 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.platformer.game.Screens.PlayScreen;
 import com.platformer.game.platformerGame;
 
@@ -20,20 +22,34 @@ public class Player extends Sprite {
     public Body body;
     public static int jump;
     protected Fixture fixture;
-    private TextureRegion playerLeftStand1;
-    private TextureRegion playerLeftStand2;
-    private TextureRegion playerRightStand1;
-    private TextureRegion playerRightStand2;
+    private enum State {FALLING, JUMPING, STANDING, RUNNING};
+    public State currentState;
+    public State previousState;
+    private Animation<TextureRegion> playerRUN;
+    private Animation<TextureRegion> playerJUMP;
+    private Animation<TextureRegion> playerSTAND;
+    private float stateTimer;
+    private boolean runningRight;
 
     public Player(World world, PlayScreen ps){
-        super(ps.getAtlas().findRegion("leftstand1"));
-        playerLeftStand1 = new TextureRegion(getTexture(), 0, 0, 64, 64);
-        playerLeftStand2 = new TextureRegion(getTexture(), 64, 0, 64, 64);
-        playerRightStand1 = new TextureRegion(getTexture(), 128, 0, 64, 64);
-        playerRightStand2 = new TextureRegion(getTexture(), 192, 0, 64, 64);
+        super(ps.getAtlas().findRegion("run1"));
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
 
-        setBounds(0, 0, 80 / platformerGame.PPM, 64 / platformerGame.PPM);
-        setRegion(playerLeftStand1);
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for(int i = 0; i < 3; i ++)
+            frames.add(new TextureRegion(getTexture(), i * 64, 0, 64, 64));
+        playerRUN = new Animation<TextureRegion>(0.2f, frames);
+        frames.clear();
+        for(int i = 3; i < 5; i ++)
+            frames.add(new TextureRegion(getTexture(), i * 64, 0, 64, 64));
+        playerSTAND = new Animation<TextureRegion>(0.2f, frames);
+        frames.clear();
+
+
+        setBounds(0, 0, 64 / platformerGame.PPM, 64 / platformerGame.PPM);
         this.world = world;
         definePlayer();
         jump = 2;
@@ -41,6 +57,48 @@ public class Player extends Sprite {
 
     public void update(float dt) {
         setPosition(body.getPosition().x - getWidth() / 2, body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(dt));
+    }
+
+    private TextureRegion getFrame(float dt) {
+        currentState = getState();
+
+        TextureRegion region;
+        switch(currentState){
+            case JUMPING:
+                region = new TextureRegion(getTexture(), 64, 0, 64, 64);
+                break;
+            case RUNNING:
+                region = playerRUN.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+                region = new TextureRegion(getTexture(), 0, 0, 64, 64);
+                break;
+            case STANDING:
+            default:
+                region = playerSTAND.getKeyFrame(stateTimer, true);
+                break;
+        }
+        if((body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()){
+            region.flip(true, false);
+            runningRight = false;
+        } else if((body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()) {
+            region.flip(true, false);
+            runningRight = true;
+        }
+        stateTimer = currentState == previousState ? stateTimer + dt : 0;
+        previousState = currentState;
+        return region;
+    }
+
+    public State getState() {
+        if(body.getLinearVelocity().y > 0)
+            return State.JUMPING;
+        else if(body.getLinearVelocity().y < 0)
+            return State.FALLING;
+        else if(body.getLinearVelocity().x != 0)
+            return State.RUNNING;
+        else return State.STANDING;
     }
 
     public void definePlayer(){
