f62172c18016f33c3233160af1f8a67d87d9c1da
Jia Ming <jma8774@bths.edu>
2017-06-07 12:37:09 -0400

    created GameOverScreen

---
+++ core/src/com/platformer/game/Screens/GameOverScreen.java
@@ -0,0 +1,89 @@
+package com.platformer.game.Screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.platformer.game.platformerGame;
+
+/**
+ * Created by Student 6 on 6/7/2017.
+ */
+
+public class GameOverScreen implements Screen {
+    private Viewport viewport;
+    private Stage stage;
+    private Game game;
+
+    public GameOverScreen(Game game) {
+        this.game = game;
+        viewport = new FitViewport(platformerGame.gameWidth, platformerGame.gameHeight, new OrthographicCamera());
+        stage = new Stage(viewport, ((platformerGame) game).batch);
+
+        Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
+        Table table = new Table();
+        table.center();
+        table.setFillParent(true);
+
+        Label gameOverLabel = new Label("GAME OVER", font);
+        Label playAgainLabel = new Label("Click to Play Again", font);
+
+        gameOverLabel.setFontScale(10);
+        playAgainLabel.setFontScale(5);
+        table.add(gameOverLabel).expand();
+        table.row();
+        table.add(playAgainLabel).expand().padTop(10f);
+
+        stage.addActor(table);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        if(Gdx.input.justTouched()) {
+            game.setScreen(new PlayScreen((platformerGame) game));
+            dispose();
+        }
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
